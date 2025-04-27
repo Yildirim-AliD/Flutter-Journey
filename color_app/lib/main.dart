2551d244d186898c9dcb1d70b25956c92427e743
Yildirim-AliD <yldrmalid@gmail.com>
2025-04-27 03:25:42 +0300

    Update_Color_App

--- color_app/lib/main.dart
+++ color_app/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
@@ -9,7 +12,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: ThemeData(), home: ColorPickerPage());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+      home: ColorPickerPage(),
+    );
   }
 }
 
@@ -22,6 +29,8 @@ class ColorPickerPage extends StatefulWidget {
 
 class _ColorPickerPageState extends State<ColorPickerPage> {
   Color selectedColor = Colors.blue;
+  bool isCircular = false;
+  bool isShowColorName = true;
   final Map<Color, String> colors = {
     Colors.red: "Red",
     Colors.blue: "Blue",
@@ -32,7 +41,42 @@ class _ColorPickerPageState extends State<ColorPickerPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Color Picker"), centerTitle: true),
+      appBar: AppBar(
+        title: Text("Color Picker"),
+        centerTitle: true,
+        actions: [
+          PopupMenuButton<String>(
+            onSelected: (value) {
+              setState(() {
+                isShowColorName = !isShowColorName;
+              });
+            },
+            itemBuilder: (context) {
+              return [
+                PopupMenuItem(
+                  value: 'a',
+                  child: Row(
+                    children: [
+                      Icon(
+                        isShowColorName
+                            ? Icons.visibility_off
+                            : Icons.visibility,
+                        size: 20,
+                        color: Colors.black54,
+                      ),
+                      const SizedBox(width: 8),
+                      Text(
+                        isShowColorName ? 'Hide Color Name' : 'Show Color Name',
+                      ),
+                    ],
+                  ),
+                ),
+              ];
+            },
+            icon: Icon(Icons.more_vert),
+          ),
+        ],
+      ),
       body: Center(
         child: Column(
           children: [
@@ -41,7 +85,7 @@ class _ColorPickerPageState extends State<ColorPickerPage> {
               height: 200,
               decoration: BoxDecoration(
                 color: selectedColor,
-                borderRadius: BorderRadius.circular(20),
+                borderRadius: BorderRadius.circular(isCircular ? 100 : 10),
                 boxShadow: [
                   BoxShadow(
                     color: selectedColor.withAlpha(150),
@@ -52,39 +96,87 @@ class _ColorPickerPageState extends State<ColorPickerPage> {
               ),
             ),
             SizedBox(height: 10),
-            Text("Blue"),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                DropdownButton<Color>(
-                  items:
-                      colors.entries.map((entry) {
-                        return DropdownMenuItem(
-                          value: entry.key,
-                          child: Row(
-                            children: [
-                              Container(
-                                width: 20,
-                                height: 20,
-                                color: entry.key,
-                              ),
-                              SizedBox(width: 5),
-                              Text(entry.value),
-                            ],
-                          ),
-                        );
-                      }).toList(),
-                  onChanged: (value) {
-                    setState(() {
-                      selectedColor = value!;
-                    });
-                  },
-                ),
-              ],
+            isShowColorName ? Text(colors[selectedColor]!) : SizedBox(),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  DropdownButton<Color>(
+                    value: selectedColor,
+                    items:
+                        colors.entries.map((entry) {
+                          return DropdownMenuItem(
+                            value: entry.key,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 20,
+                                  height: 20,
+                                  color: entry.key,
+                                ),
+                                SizedBox(width: 5),
+                                Text(entry.value),
+                              ],
+                            ),
+                          );
+                        }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        selectedColor = value!;
+                      });
+                    },
+                  ),
+                  ElevatedButton(
+                    onPressed: _RandomColor,
+                    child: const Text("Random Color"),
+                  ),
+                  IconButton(
+                    onPressed: _showColorCode,
+                    icon: const Icon(Icons.info),
+                  ),
+                  IconButton(
+                    onPressed: _changeShape,
+                    icon: Icon(
+                      isCircular
+                          ? Icons.square_outlined
+                          : Icons.circle_outlined,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  void _RandomColor() {
+    final colors2 = colors.keys.toList();
+    final randomNumber = Random().nextInt(colors2.length);
+    final randomColor = colors2[randomNumber];
+    setState(() {
+      selectedColor = randomColor;
+    });
+  }
+
+  void _showColorCode() {
+    Fluttertoast.showToast(
+      msg:
+          "RGB : (${selectedColor.red},${selectedColor.green},${selectedColor.blue})",
+      toastLength: Toast.LENGTH_LONG,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      backgroundColor: selectedColor,
+      textColor: Colors.white,
+      fontSize: 16.0,
+    );
+  }
+
+  void _changeShape() {
+    setState(() {
+      isCircular = !isCircular;
+    });
   }
 }
